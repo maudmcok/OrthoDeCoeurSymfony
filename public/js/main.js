59b2bf9e98a9d0c4ae6a5b8019007fcb44c37a99
Joanne Lyon <joannelyon@macbookpro.home>
2025-03-19 04:53:17 +0100

    Fix Slider

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".slider");
+
+    slider.addEventListener("mouseenter", function() {
+        slider.style.animationPlayState = "paused";
+    });
+
+    slider.addEventListener("mouseleave", function() {
+        slider.style.animationPlayState = "running";
+    });
+});
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,23 @@ final class MainController extends AbstractController{
     #[Route('', name: 'app_main')]
     public function index(): Response
     {
+        $slides = [
+            ['image' => 'images/assurances/ankara.png', 'name' => 'ANKARA'],
+            ['image' => 'images/assurances/gga.png', 'name' => 'G G A'],
+            ['image' => 'images/assurances/madgi.jpeg', 'name' => 'MADGI'],
+            ['image' => 'images/assurances/mspci.png', 'name' => 'MSPCI'],
+            ['image' => 'images/assurances/vitasante.jpeg', 'name' => 'VITASANTE'],
+            ['image' => 'images/assurances/olea.jpeg', 'name' => 'OLEA SANTÉ'],
+            ['image' => 'images/assurances/chorus.jpeg', 'name' => 'CHORUS'],
+            ['image' => 'images/assurances/atlantique.png', 'name' => 'ATLANTIQUE ASSURANCES'],
+            ['image' => 'images/assurances/cimef.png', 'name' => 'CIMEF ASSURANCES'],
+            ['image' => 'images/assurances/vitalis.png', 'name' => 'VITALIS SANTÉ'],
+            ['image' => 'images/assurances/nsia.jpeg', 'name' => 'NSIA ASSURANCES'],
+        ];
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'slides' => $slides,
         ]);
     }
 }
